c134eb6f77aa990a3b715bc089fba42f6af50b7c
GlobalFintechPartners <95564380+GlobalFintechPartners@users.noreply.github.com>
2021-12-05 11:55:16 +0100

    Delete Online_Workspace_GFP directory

--- Online_Workspace_GFP/token_ERC-1155.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract TT is ERC20 {
-    constructor(uint256 initialSupply) public ERC20("TestToken", "TT") {
-        _mint(msg.sender, initialSupply);
-    }
-}d
